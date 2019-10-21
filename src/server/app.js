4853d776b9fb6bc9d8929fb2fd760d154685bc4e
Anton <anton.odman@gmail.com>
2019-10-21 20:23:12 +0200

    Added Helmet

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+
 const middleware = require('./utils/middleware');
 const apiRouter = require('./controller/apiRouter');
 require('dotenv').config();
@@ -13,6 +15,7 @@ mongoose
     .catch(error => console.log('Could not connect to MongoDB: ', error.message));
 
 app.use(bodyParser.json());
+app.use(helmet());
 app.use(express.static('dist'));
 
 app.use('/api', apiRouter);
